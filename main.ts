4408ab21ab5acbdf2d7745b21c64fc9ffa6a767d
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-03-10 13:27:20 -0400

    add functions

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Dat Nguyen
+ * Created on: Mar 2026
+ * This program will run Rock, Paper, Scissors.
 */
 
-basic.showString('Hello, World!')
+let randomNumber: number = null
+
+input.onGesture(Gesture.Shake, function() {
+    randomNumber = randint(0, 2)
+    if (randomNumber == 0) {
+        displa
+    }
+    if (randomNumber == 1) {
+
+    }
+    if (randomNumber == 2) {
+
+    }
+})
+
+input.onButtonPressed(Button.A, function() {
+    
+})
+
+input.onButtonPressed(Button.A, function() {
+    
+})
